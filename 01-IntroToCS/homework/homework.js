11bec3e5a350c135b12c04278f9bcb08660d86b8
joelorzet <joelorzt@gmail.com>
2022-06-06 17:58:44 -0300

    Intro to CS completed, test passed

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,17 +1,36 @@
-'use strict'
+'use strict';
 
 function BinarioADecimal(num) {
-  // tu codigo aca
+	// tu codigo aca
+	const binary = String(num);
+	let exponent = 0;
+	let decimalNumber = 0;
 
+	for (let i = binary.length; i > 0; i--) {
+		if (binary[i - 1] === '1') {
+			decimalNumber += 2 ** exponent;
+		}
+		exponent += 1;
+	}
+
+	return decimalNumber;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
+	// tu codigo aca
+	let binario = '';
+	let newNum = num;
 
+	while (newNum > 0) {
+		binario += newNum % 2;
+
+		newNum = Math.floor(newNum / 2);
+	}
+
+	return binario.split('').reverse().join('');
 }
-
 
 module.exports = {
-  BinarioADecimal,
-  DecimalABinario,
-}
+	BinarioADecimal,
+	DecimalABinario,
+};
